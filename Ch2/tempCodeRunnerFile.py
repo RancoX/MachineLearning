ab70a9939ad06053da603ea46a0579eac049d7fd
Ranco Xu <rancoxu@gmail.com>
2022-10-07 20:52:18 +1000

    First commit

---
+++ Ch2/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+
+        # for idx, cl in enumerate(np.unique(y)):
+        #     plt.scatter(x=X[y==cl,0],y=X[y==cl,1],alpha=0.8, c=colors[idx],marker=markers[idx],label=f"Class {cl}",edgecolors='black')
